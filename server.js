7c7606e6afb942e60d25292a01c9931a176cc36b
jjhosuepinoza <97245422+jjhosuepinoza@users.noreply.github.com>
2023-03-25 00:33:05 +0100

    commit

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongodb = require('./db/connect');
+// const { application } = require('express');
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5050;
 const app = express();
 
 app
   .use(bodyParser.json())
   .use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'https://cse341-contacts-frontend.netlify.app');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+  );
+  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT,DELETE, OPTIONS'); 
+
+  
+  
     next();
   })
   .use('/', require('./routes'));
