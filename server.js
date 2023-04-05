e9eaf7a6467dcfe8fa957a78037faa25518e1161
jjhosuepinoza <97245422+jjhosuepinoza@users.noreply.github.com>
2023-04-06 01:12:56 +0200

    commit

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongodb = require('./db/connect');
+const axios = require('axios');
+const path = require('path');
+
 
 const port = process.env.PORT || 5050;
 const app = express();
@@ -20,4 +23,48 @@ mongodb.initDb((err) => {
     app.listen(port);
     console.log(`Connected to DB and listening on ${port}`);
   }
+
+
+
+  app.use(express.static('static'));
+
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/static/index.html'));
+  });
+  
+  app.get('/auth', (req, res) => {
+    res.redirect(
+      `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`,
+    );
+  });
+  
+  app.get('/oauth-callback', ({ query: { code } }, res) => {
+    const body = {
+      client_id: process.env.GITHUB_CLIENT_ID,
+      client_secret: process.env.GITHUB_SECRET,
+      code,
+    };
+    const opts = { headers: { accept: 'application/json' } };
+    axios
+      .post('https://github.com/login/oauth/access_token', body, opts)
+      .then((_res) => _res.data.access_token)
+      .then((token) => {
+        // eslint-disable-next-line no-console
+        console.log('My token:', token);
+  
+        res.redirect(`/?token=${token}`);
+      })
+      .catch((err) => res.status(500).json({ err: err.message }));
+  });
+
+
+
 });
+
+
+
+
+
+app.listen(3000);
+// eslint-disable-next-line no-console
+console.log('App listening on port 3000');
